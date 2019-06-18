8816ea39a7492730a261848e855884be34b9a2d4
VictorHongDream <45105361+VictorHongDream@users.noreply.github.com>
2019-06-18 17:23:40 +0800

    Update gg.c

--- gg.c
+++ gg.c
@@ -1,5 +1,33 @@
-BFS_Ring_LCA():
-	BFS_to_Tree() //通过BFS生成BFS树并且获得树边
-	getElseEdges() //获取所有的非树边
-	findLCA_Union() //对所有非树边进行环集合的归类
-	getRingFromTreeEdges() //从树边里面判断两点是否属于同一个环来获取桥
+findLCA_Union():  //对每条非树边进行环集合处理
+	for eachEdge in ElseEdges:
+		u = eachEdge.a
+		v = eachEdge.b   //获取边的端点
+		vector<int> temp    //临时容器存储路径上经过环的点
+		//从深度判断，不断递减直到找到它们最近的公共祖先
+	    if depth[v] == depth[u]:  
+	    	temp.push_back(u)
+	        temp.push_back(v)
+	 	else if depth[v] > depth[u]:
+	        while depth[v] > depth[u]:
+	        	temp.push_back(v)
+	            v = father[v]
+	        temp.push_back(u)
+		else   
+			while depth[v] < depth[u]:
+	        	temp.push_back(u)
+	            u = father[u]
+			temp.push_back(v)
+		while (father[u] != father[v]):
+	        u = father[u]
+	        v = father[v]
+	        temp.push_back(u)
+	        temp.push_back(v)
+	    int f = father[u]
+	    while sig[f] != f:
+	        f = sig[f]
+	    sig[u] = f
+	    sig[v] = f
+	    //将路径上的点都设置为同一个标志
+	    for i = 0 to temp.size():
+	        if(temp[i] != f)
+	            sig[temp[i]] = f
