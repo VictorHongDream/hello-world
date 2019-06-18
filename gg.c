97f9c85f09ed65d1263e2373d0e1f2a41cd3cb16
VictorHongDream <45105361+VictorHongDream@users.noreply.github.com>
2019-06-18 17:21:55 +0800

    Update gg.c

--- gg.c
+++ gg.c
@@ -1,16 +1,5 @@
-void tarjan(int S):
-	Stack.push(S); // 把S放入栈中。
-	Vis[S] = 1;// 表示S节点在栈中。
-    DFN[S] = LOW[S] = ++FLAG // 初始化时间戳及先祖地位。
-	For( T 属于 S的son) // 遍历所有S的子节点。
-	If(vis[T] == 0): // 如果 T 不在栈中
-		Tarjan(T);  //继续向下搜索。
-		LOW[S] = MIN(LOW[S], LOW[T]). //更新S节点的先祖地位。
-	Else  if(vis[T] == 1): //S节点 正在栈中。
-		LOW[S] = MIN(LOW[S], DFN[T]). // 更新S节点的先祖地位。
-	If(LOW[S] == DNF[S]): // 如果 S节点的先祖地位就是他自己 那么他就是一个强连通分量的ROOT。                                            
-	DO{
-		Pop T //  弹出T
-		Vis[T] = 2; // 表示T节点已经进入过并弹出
-		T = stack.top; //更新T
-		}While(S != T) // 一直弹到本强连通分量的ROOT为止。 弹出的节点便是一个强连通分量。
+BFS_Ring_LCA():
+	BFS_to_Tree() //通过BFS生成BFS树并且获得树边
+	getElseEdges() //获取所有的非树边
+	findLCA_Union() //对所有非树边进行环集合的归类
+	getRingFromTreeEdges() //从树边里面判断两点是否属于同一个环来获取桥
